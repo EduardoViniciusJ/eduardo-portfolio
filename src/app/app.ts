4d3213ce41509edc24a5333ddcf169a91bd5a832
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-17 12:20:16 -0300

    feat: add components in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar-component/navbar-component';
+import { MainComponent } from './components/main-component/main-component';
+import { FooterComponent } from './components/footer-component/footer-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, MainComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
